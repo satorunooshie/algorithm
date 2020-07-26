da6cbe5f7193c4cbf236efc8e8bb1c60f7470d71
satorunooshie <rule.the.fate.myfirststory@gmail.com>
2020-07-26 15:19:32 +0900

    ユークリッドの互除法改善版

--- euclidean.php
+++ euclidean.php
@@ -1,7 +1,9 @@
 <?php
 //二つの数値の最大公約数を求める
+
 $m = 28;
 $n = 77;
+//--------------------------------------------------
 function euclidean($m, $n) {
 	if ($m < $n) {
 		$r = $m;
@@ -26,4 +28,18 @@ function euclidean($m, $n) {
 	}
 	return euclidean($n, $m % $n);
 }
-echo euclidean($n, $m);
+echo euclidean($n, $m) . "\n";
+//-------------------------------------------
+echo '一つ目' . $m . "\n";
+echo '二つ目' . $n . "\n";
+if ($m > $n) {
+	$tmp = $m;
+	$m = $n;
+	$n = $tmp;
+}
+do {
+	$d = $m % $n;
+	$m = $n;
+	$n = $d;
+} while ($d != 0);
+echo "二つの数の最大公約数は$m\n";
