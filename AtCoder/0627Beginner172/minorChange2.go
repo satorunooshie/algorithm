ffbd06f13b820d597e54e421cd05f52612b5f568
satorunooshie <rule.the.fate.myfirststory@gmail.com>
2020-08-05 23:25:47 +0900

    go ver

---
+++ AtCoder/0627Beginner172/minorChange2.go
@@ -0,0 +1,30 @@
+package main
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
+const (
+    initialBufSize = 1e4
+    maxBufSize = 1e6 + 7
+)
+var buf []byte = make([]byte, maxBufSize)
+var sc = bufio.NewScanner(os.Stdin)
+func init() {
+    //sc.Split(bufio.ScanWords)
+    sc.Buffer(buf, maxBufSize)
+}
+func main() {
+    S, T := nextLine(), nextLine()
+    var ans int
+    for i := 0; i < len(S); i++ {
+        if S[i] != T[i] {
+            ans++
+        }
+    }
+    fmt.Println(ans)
+}
+func nextLine() string {
+    sc.Scan()
+    return sc.Text()
+}
