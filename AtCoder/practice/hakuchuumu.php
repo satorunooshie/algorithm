dfe6fbc7ac2b6b0a245e4c146ffaf7245a4b069d
satorunooshie <rule.the.fate.myfirststory@gmail.com>
2020-08-05 17:25:13 +0900

    str_replace

---
+++ AtCoder/practice/hakuchuumu.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * 英小文字からなる文字列Sが与えられたとき
+ * Tが空文字列である状態から始め
+ * 以下の操作を好きな回数繰り返すことでS=Tとなるか検証
+ * dream, dreamer, erase, eraser
+ * 1<=S<=10^5
+ */
+$string = trim(fgets(STDIN));
+$string = str_replace('eraser', '', $string);
+$string = str_replace('erase', '', $string);
+$string = str_replace('dreamer', '', $string);
+$string = str_replace('dream', '', $string);
+if ($string == '') {
+	echo "YES";
+} else {
+	echo "NO";
+}
