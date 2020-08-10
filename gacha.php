447e997d6bb2fd0792528fc7b404e5fcc4eb2702
satorunooshie <rule.the.fate.myfirststory@gmail.com>
2020-08-11 01:49:26 +0900

    10連

--- gacha.php
+++ gacha.php
@@ -19,8 +19,38 @@ $probability = 0;
 foreach ($raritys as $rarity => $rarity_probability) {
 	$probability += $rarity_probability;
 	if ($rand <= $probability) {
-		$result = array_rand($cards[$rarity], 1);
+		$res = array_rand($cards[$rarity], 1);
 		break;
 	}
 }
-echo $cards[$rarity][$result];
+echo $cards[$rarity][$res];
+echo "\n";
+//-------------------------------------------
+//10連ガチャ
+$sr_raritys = [
+	'SSR' => 300,
+	'SR' => 9700,
+];
+$rand = mt_rand(0, 10000);
+for ($i = 0; $i < 9; $i++) {
+	$probability = 0;
+	foreach ($raritys as $rarity => $rarity_probability) {
+		$probability += $rarity_probability;
+		if ($rand <= $probability) {
+			$result[] = array_rand($cards[$rarity], 1);
+			break;
+		}
+	}
+}
+//SR以上確定
+$probability = 0;
+foreach ($sr_raritys as $rarity => $rarity_probability) {
+	$probability += $rarity_probability;
+	if ($rand <= $probability) {
+		$result[] = array_rand($cards[$rarity], 1);
+		break;
+	}
+}
+foreach ($result as $v) {
+	echo $v . "\n";
+}
