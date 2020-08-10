b7654f66acb7a75cb6babe63010785b100e4dc82
satorunooshie <rule.the.fate.myfirststory@gmail.com>
2020-08-11 02:35:41 +0900

    別のバージョン

---
+++ gacha2.php
@@ -0,0 +1,45 @@
+<?php
+$gacha = [
+	'0' => [
+		'name' => 'a',
+		'percent' => 12,
+	],
+	'1' => [
+		'name' => 'b',
+		'percent' => 1,
+	],
+	'2' => [
+		'name' => 'c',
+		'percent' => 67,
+	],
+	'3' => [
+		'name' => 'd',
+		'percent' => 10,
+	],
+	'4' => [
+		'name' => 'e',
+		'percent' => 5,
+	],
+	'5' => [
+		'name' => 'f',
+		'percent' => 5,
+	],
+];
+/*
+ * bad answer
+ * $hitKey = rand(0, count($gacha));
+ * echo $gacha[$hitKey];
+ * unset($gacha[$hitKey]);
+ */
+
+$hitRand = rand(0, 100);
+echo $hitRand . "\n";
+$sumPer = 0;
+for ($i = 0; $i < count($gacha); $i++) {
+	$sumPer += $gacha[$i]['percent'];
+	$hitItem = $gacha[$i];
+	if ($hitRand < $sumPer) {
+		break;
+	}
+}
+	var_dump($hitRand);
