97f3c4dee93d1040655c24faf5de80233a86b4e9
satorunooshie <rule.the.fate.myfirststory@gmail.com>
2020-08-22 18:10:15 +0900

    階乗の計算を再帰を使って行う

---
+++ factorial.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * 階乗の計算を再帰を使って求める
+ */
+var_dump(calculateFactorial(1));
+echo "\n";
+var_dump(calculateFactorial(4));
+function calculateFactorial($n) {
+	if ($n == 1) {
+		return 1;
+	}
+	return $n * calculateFactorial($n - 1);
+}
