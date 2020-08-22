1d4daa65eb15e419463ad394e9f3fd1843473bd4
satorunooshie <rule.the.fate.myfirststory@gmail.com>
2020-08-22 17:00:30 +0900

    二分å木

---
+++ binaryTreeTest.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ * 二文木のテスト
+ */
+
+require_once './part/TreeNode.php';
+require_once './part/BinaryTree.php';
+
+$tree = new TreeNode();
+$tree->value = 1000;
+BinaryTree::insertNode(2000, $tree);
+BinaryTree::insertNode(300, $tree);
+BinaryTree::insertNode(4000, $tree);
+BinaryTree::insertNode(5000, $tree);
+BinaryTree::insertNode(100, $tree);
+BinaryTree::insertNode(50, $tree);
+BinaryTree::insertNode(10, $tree);
+BinaryTree::insertNode(200, $tree);
+BinaryTree::insertNode(400, $tree);
+echo "\n";
+var_dump($tree);
+echo "\n";
+echo "ここから検索テスト";
+var_dump(BinaryTree::find(1000, $tree));
+var_dump(BinaryTree::find(2000, $tree));
+var_dump(BinaryTree::find(300, $tree));
+var_dump(BinaryTree::find(4000, $tree));
+var_dump(BinaryTree::find(5000, $tree));
+var_dump(BinaryTree::find(100, $tree));
+var_dump(BinaryTree::find(50, $tree));
+var_dump(BinaryTree::find(10, $tree));
+var_dump(BinaryTree::find(200, $tree));
+var_dump(BinaryTree::find(400, $tree));
+var_dump(BinaryTree::find(1, $tree));
+echo "\n";
+echo "ここから消去テスト";
+echo "\n";
+var_dump(BinaryTree::delete(1, $tree));
+echo "\n";
+/*
+ * リーフの削除テスト
+ * var_dump(BinaryTree::delete(400, $tree));
+ * 右のノードしかないときの削除テスト
+ * var_dump(BinaryTree::delete(2000, $tree));
+ * 左のノードしかないときの削除テスト
+ * var_dump(BinaryTree::delete(50, $tree));
+ * 右と左のノードがあるときのテスト
+ * var_dump(BinaryTree::delete(300, $tree));
+ * 右と左のノードがあるときのテスト
+ * var_dump(BinaryTree::delete(400, $tree));
+ * ルートの削除テスト
+ * var_dump(BinaryTree::delete(1000, $tree));
+ * 木を左のノードにしかないようにしてルートを削除
+ * 木を右のノードにしてルートを削除
+ */
+var_dump(BinaryTree::delete(1000, $tree));
+echo "\n";
+var_dump($tree);
+echo "\n";
---
+++ part/BinaryTree.php
@@ -0,0 +1,130 @@
+<?php
+/*
+ * バイナリ―ツリー
+ */
+class BinaryTree {
+	public static function createNewNode($value) {
+		$node = new TreeNode();
+		$node->value = $value;
+		$node->leftNode = null;
+		$node->rightNode = null;
+		return $node;
+	}
+	public static function InsertNode($value, $node) {
+		if ($node->value == $value) {
+			return true;
+		}
+		if ($node->value == $value) {
+			//ノードの値より小さいとき、左のこのノードに
+			if ($node->leftNode != null) {
+				//すでに左のノードにデータがあるとき次のノードに進む
+				self::insertNode($value, $node->leftNode);
+			} else {
+				//左のノードにデータを挿入
+				$node->leftNode = self::createNewNode($value);
+				return true;
+			}
+		} else {
+			//ノードの値より大きいとき、右のこのノードに
+			if ($node->rightNode != null) {
+				//既に右のノードにデータがある場合次のノードに進む
+				self::insertNode($value, $node->rightNode);
+			} else {
+				//右のノードにデータを挿入
+				$node->rightNode = self::createNewNode($value);
+				return true;
+			}
+		}
+	}
+	public static function find($value, $node) {
+		if ($node->value == $value) {
+			//値が見つかったとき
+			return true;
+		}
+		if ($node->value > $value) {
+			if (empty($node->leftNode)) {
+				return false;
+			}
+			//探している値が対象のノードより小さいときは左のノードへん
+			return self::find($value, $node->leftNode);
+		} else {
+			if (empty($node->rightNode)) {
+				return false;
+			}
+			//右のノードへ
+			return self::find($value, $node->rightNode);
+		}
+	}
+	public static function delete($value, &$tree) {
+		$rootNode = &$tree; //ルートノードのアドレス
+		$node = &$node; //一個ずつしらべ最後は削除対象のノード
+		$parent = null; //$nodeの親のノードアドレス
+		$direction = 0; //根の場合は０、左の子に進むときはー１を右の時は１
+		//削除対象のノードを検索
+		while ($node !== null && $node->value != $value) {
+			if ($node->value > $value) {
+				$parent = $node;
+				$node = &$node;
+				$node = &$node->leftNode;
+				$direction = -1;
+			} else {
+				$parent = &$node;
+				$node = &$node->rightNode;
+				$direction = -1;
+			}
+		}
+		//削除対象が木になかった時
+		if ($node == null) {
+			return false;
+		}
+		//削除対象のノードがあるとき
+		if ($node->leftNode == null && $node->rightNode == null) {
+			//右と左のノードがないとき
+			//つまりリーフを削除
+			if ($direction == -1) {
+				$parent->leftNode = null;
+			} else {
+				$parent->rightNode = null;
+			}
+		} elseif ($node->leftNode == null && $node->rightNode != null) {
+			//左のノードがなくかつ右のノードがあるとき
+			if ($direction == -1) {
+				$parent->leftNode = $node->leftNode;
+			} elseif ($direction == 1) {
+				$parent->rightNode = $node->rightNode;
+			} elseif ($direction == 0) {
+				//木構造で左のノードしかなくルートを削除するとき
+				$rootNode = $node->rightNode;
+			}
+		} elseif ($node->leftNode != null && $node->rightNode == null) {
+			//左のノードがあってかつ右のノードがないとき
+			if ($direction == -1) {
+				$parent->leftNode = $node->leftNode;
+			} elseif ($direction == 1) {
+				$parent->rightNode = $node->rightNode;
+			} elseif ($direction == 0) {
+				//木構造で左のノードしかなくルートを削除するとき
+				$rootNode = $node->leftNode;
+			}
+		} else {
+			//右と左の子がある場合
+			//左のノードの最大値と削除したいノードと交換する
+			$leftBiggest = &$node->leftNode;
+			$parent = &$node;
+			$direction = -1;
+			//右のノードがある限り上書きしていく
+			while ($leftBiggest->rightNode != null) {
+				$parent = &$leftBiggest;
+				$leftBiggest = &$leftBiggest->rightNode;
+				$direction = 1;
+			}
+			$node->value = $leftBiggest->value;
+			if ($direction == -1) {
+				$parent->leftNode = &$leftBiggest->leftNode;
+			} else {
+				$parent->rightNode = &$leftBiggest->leftNode;
+			}
+		}
+		return true;
+	}
+}
