aeba5cda09b825c63f58398d3f7e31740be40624
satorunooshie <rule.the.fate.myfirststory@gmail.com>
2020-08-24 00:18:01 +0900

    kotlin

---
+++ AtCoder/keyence2020/robotArms2.kt
@@ -0,0 +1,22 @@
+fun longList() = readLine()?.split(" ")?.map(String::toLong) ?: TODO()
+fun main(args: Array<String>) {
+    val n = readLine()?.toInt() ?: return
+    var c: Pair<Long, Long>? = null
+    var count = 1
+    0.util(n).map {
+        val (x, l) = longList()
+        Pair(x - l, x + l)
+    }
+        .sortedBy { it.second }
+        .forEach { next ->
+            c?.let {
+                if (it.second <= next.first) {
+                    count++
+                    c = next
+                }
+            } ?: run {
+                c = next
+            }
+        }
+    println(count.toString())
+}
