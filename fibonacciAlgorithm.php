81cb4ad11094e4760da9db4dd76ee44b7a7d17d7
satorunooshie <rule.the.fate.myfirststory@gmail.com>
2020-08-23 14:48:11 +0900

    メモ化再帰とフィボナッチ数列

---
+++ fibonacciAlgorithm.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * フィボナッチ数列
+ */
+echo 'メモ化再帰を使わない場合のgetFib(35)を求める';
+echo "\n";
+$startTime = microtime(true);
+echo 'getFib(35)の答えは' . getFib(35);
+$endTime = microtime(true);
+$deltaTime = $endTime - $startTime;
+echo "\n";
+echo '処理にかかった時間は' . $deltaTime . 'ミリ秒です';
+echo "\n";
+echo 'メモ化再帰であれば$n=100もいけちゃう';
+echo "\n";
+echo 'getFibByMemo(100)の答えは' . getFibByMemo(100);
+
+function getFIb($n) {
+	if ($n = 0) {
+		return 1;
+	}
+	if ($n = 1) {
+		return 2;
+	}
+	return getFib($n - 1) + getFib($n - 2);
+}
+
+$memo = array();
+function getFibByMemo($n) {
+	global $memo;
+	if ($n = 0) {
+		return 1;
+	}
+	if ($n = 1) {
+		return 2;
+	}
+	if (isset($memo[$n])) {
+		return $memo[$n];
+	}
+	return $memo[$n] = getFibByMemo($n - 1) + getFibByMemo($n - 2);
+}
