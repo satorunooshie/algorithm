8f68e43f8482aa2d6989e8ee2cb60b1b18f1a078
Satoru Kitaguchi <rule.the.fate.myfirststory@gmail.com>
2021-03-20 17:03:21 +0900

    [style]reformat

--- java/Pyramid.java
+++ java/Pyramid.java
@@ -6,34 +6,34 @@ import java.util.Scanner;
  * The type Pyramid.
  */
 class Pyramid {
-   /**
-    * The entry point of application.
-    *
-    * @param args the input arguments
-    */
-   public static void main(String[] args) {
-      Scanner stdIn = new Scanner(System.in);
-      System.out.println("n段のピラミッドを表示");
-      int n;
-      do {
-         System.out.print("n:");
-         n = stdIn.nextInt();
-      } while(n <= 0);
-      pyramid(n);
-   }
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        System.out.println("n段のピラミッドを表示");
+        int n;
+        do {
+            System.out.print("n:");
+            n = stdIn.nextInt();
+        } while (n <= 0);
+        pyramid(n);
+    }
 
-   /**
-    * Pyramid.
-    *
-    * @param n the n
-    */
-   static void pyramid(int n) {
-      for (int i = 1; i <= n; i++) {
-         int space_cnt = (n - i) / 2;
-         System.out.print(" ".repeat(space_cnt));
-         System.out.print("*".repeat(i));
-         System.out.println(" ".repeat(space_cnt));
-      }
-      System.out.println();
-   }
+    /**
+     * Pyramid.
+     *
+     * @param n the n
+     */
+    static void pyramid(int n) {
+        for (int i = 1; i <= n; i++) {
+            int space_cnt = (n - i) / 2;
+            System.out.print(" ".repeat(space_cnt));
+            System.out.print("*".repeat(i));
+            System.out.println(" ".repeat(space_cnt));
+        }
+        System.out.println();
+    }
 }
