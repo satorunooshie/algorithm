27f0b93badef0f8090d233f4dadac7727cbba372
Satoru Kitaguchi <rule.the.fate.myfirststory@gmail.com>
2021-03-20 20:53:18 +0900

    [docs]オープンアドレス法によるハッシュ(衝突時再ハッシュ)

--- java/OpenHash.java
+++ java/OpenHash.java
@@ -1,40 +1,23 @@
-public class OpenHash<K, V> {
-    enum Status {
-        OCCUPIED,
-        EMPTY,
-        DELETED;
-    };
-    static class Bucket<K, V> {
-        private K key;
-        private V data;
-        private Status stat;
-        Bucket() {
-            //バケットは空
-            stat = Status.EMPTY;
-        }
-        void set(K key, V data, Status stat) {
-            this.key = key;
-            this.data = data;
-            this.stat = stat;
-        }
-        void setStat(Status stat) {
-            this.stat = stat;
-        }
-        K getKey() {
-            return key;
-        }
-        V getData() {
-            return data;
-        }
-        public int hashCode() {
-            return key.hashCode();
-        }
-    }
+package java;
 
+/**
+ * The type Open hash.
+ *
+ * @param <K> the type parameter
+ * @param <V> the type parameter
+ */
+public class OpenHash<K, V> {
     //ハッシュ表の大きさ
     private int size;
+
     //ハッシュ表
     private Bucket<K, V>[] table;
+
+    /**
+     * Instantiates a new Open hash.
+     *
+     * @param size the size
+     */
     public OpenHash(int size) {
         try {
             table = new Bucket[size];
@@ -45,12 +28,27 @@ public class OpenHash<K, V> {
             this.size = 0;
         }
     }
+
+    /**
+     * Hash value int.
+     *
+     * @param key the key
+     * @return the int
+     */
     public int hashValue(Object key) {
         return key.hashCode() % size;
     }
+
+    /**
+     * Rehash value int.
+     *
+     * @param hash the hash
+     * @return the int
+     */
     public int rehashValue(int hash) {
         return (hash + 1) % size;
     }
+
     private Bucket<K, V> searchNode(K key) {
         int hash = hashValue(key);
         Bucket<K, V> p = table[hash];
@@ -62,6 +60,13 @@ public class OpenHash<K, V> {
         }
         return null;
     }
+
+    /**
+     * Search v.
+     *
+     * @param key the key
+     * @return the v
+     */
     public V search(K key) {
         Bucket<K, V> p = searchNode(key);
         if (p != null)
@@ -69,6 +74,14 @@ public class OpenHash<K, V> {
         else
             return null;
     }
+
+    /**
+     * Add int.
+     *
+     * @param key  the key
+     * @param data the data
+     * @return the int
+     */
     public int add(K key, V data) {
         if (search(key) != null)
             //登録済み
@@ -86,6 +99,13 @@ public class OpenHash<K, V> {
         //ハッシュ表が満杯
         return 2;
     }
+
+    /**
+     * Remove int.
+     *
+     * @param key the key
+     * @return the int
+     */
     public int remove(K key) {
         Bucket<K, V> p = searchNode(key);
         if (p == null)
@@ -93,6 +113,10 @@ public class OpenHash<K, V> {
         p.setStat(Status.DELETED);
         return 0;
     }
+
+    /**
+     * Dump.
+     */
     public void dump() {
         for (int i = 0; i < size; i++) {
             System.out.printf("%02d ", i);
@@ -107,6 +131,88 @@ public class OpenHash<K, V> {
                     System.out.println("-- 削除済み --");
                     break;
             }
+        }
+    }
+
+    /**
+     * The enum Status.
+     */
+    enum Status {
+        /**
+         * Occupied status.
+         */
+        OCCUPIED,
+        /**
+         * Empty status.
+         */
+        EMPTY,
+        /**
+         * Deleted status.
+         */
+        DELETED
+    }
+
+    /**
+     * The type Bucket.
+     *
+     * @param <K> the type parameter
+     * @param <V> the type parameter
+     */
+    static class Bucket<K, V> {
+        private K key;
+        private V data;
+        private Status stat;
+
+        /**
+         * Instantiates a new Bucket.
+         */
+        Bucket() {
+            //バケットは空
+            stat = Status.EMPTY;
+        }
+
+        /**
+         * Set.
+         *
+         * @param key  the key
+         * @param data the data
+         * @param stat the stat
+         */
+        void set(K key, V data, Status stat) {
+            this.key = key;
+            this.data = data;
+            this.stat = stat;
+        }
+
+        /**
+         * Sets stat.
+         *
+         * @param stat the stat
+         */
+        void setStat(Status stat) {
+            this.stat = stat;
+        }
+
+        /**
+         * Gets key.
+         *
+         * @return the key
+         */
+        K getKey() {
+            return key;
+        }
+
+        /**
+         * Gets data.
+         *
+         * @return the data
+         */
+        V getData() {
+            return data;
+        }
+
+        public int hashCode() {
+            return key.hashCode();
         }
     }
 }
