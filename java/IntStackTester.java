6ce1857930caff8787658271d0a5c99f173192c1
Satoru Kitaguchi <rule.the.fate.myfirststory@gmail.com>
2021-03-21 19:25:58 +0900

    [add]intStackクラスの全メソッドを実装

--- java/IntStackTester.java
+++ java/IntStackTester.java
@@ -18,7 +18,7 @@ class IntStackTester {
         IntStack s = new IntStack(64);
         while (true) {
             System.out.printf("現在のデータ数: %d / %d\n", s.size(), s.getCapacity());
-            System.out.print("(1)push (2)pop (3)peek (4)dump (5)terminate:");
+            System.out.print("(1) push (2) pop (3) peek (4) dump (5) search (6) clear (7) show (0) terminate :");
 
             int menu = stdIn.nextInt();
             if (menu == 0) break;
@@ -52,6 +52,24 @@ class IntStackTester {
                     break;
                 case 4:
                     s.dump();
+                    break;
+                case 5:
+                    System.out.print("探索するデータ:");
+                    x = stdIn.nextInt();
+                    int n = s.indexOf(x);
+                    if (n >= 0)
+                        System.out.printf("頂上から%s番目に存在します", s.size() - n);
+                    else
+                        System.out.println("そのデータはありません");
+                    break;
+                case 6:
+                    s.clear();
+                    break;
+                case 7:
+                    System.out.println("容量:" + s.getCapacity());
+                    System.out.println("データ数:" + s.size());
+                    System.out.println("空" + (s.isEmpty() ? "です" : "ではありません"));
+                    System.out.println("満杯" + (s.isFull() ? "です" : "ではありません"));
                     break;
             }
         }
