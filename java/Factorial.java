c2a66271ed39b113de412a4cde685aaf689c0571
Satoru Kitaguchi <rule.the.fate.myfirststory@gmail.com>
2021-03-22 01:20:25 +0900

    [docs]階乗値を再帰的に求める

--- java/Factorial.java
+++ java/Factorial.java
@@ -1,12 +1,29 @@
+package java;
+
 import java.util.Scanner;
 
+/**
+ * The type Factorial.
+ */
 class Factorial {
+    /**
+     * Factorial int.
+     *
+     * @param n the n
+     * @return the int
+     */
     static int factorial(int n) {
         if (n > 0)
             return n * factorial(n - 1);
         else
             return 1;
     }
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
         System.out.print("整数を入力せよ:");
