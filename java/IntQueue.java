7ffe218d4d0a6a1f6d4ed352b5c19bb4fbcba1ed
Satoru Kitaguchi <rule.the.fate.myfirststory@gmail.com>
2021-03-21 22:21:10 +0900

    [docs]int型固定長Queueの実装

--- java/IntQueue.java
+++ java/IntQueue.java
@@ -1,22 +1,26 @@
+package java;
+
+/**
+ * The type Int queue.
+ */
 public class IntQueue {
-    //キュー用の配列
+    // キュー用の配列
     private int[] que;
     private int capacity;
-    //先頭要素カーソル
+    // 先頭要素カーソル
     private int front;
-    //末尾要素カーソル
+    // 末尾要素カーソル
     private int rear;
-    //現在のデータ数
-    //fornt/rearの値が等しくなったときにキューが空なのか満杯なのかが区別できなくなるのを防ぐため
-    //キューが空の時は0で、満杯の時はcapacityと同じ値になる
+    // 現在のデータ数
+    // fornt/rearの値が等しくなったときにキューが空なのか満杯なのかが区別できなくなるのを防ぐため
+    // キューが空の時は0で、満杯の時はcapacityと同じ値になる
     private int num;
 
-    public class EmptyIntQueueException extends RuntimeException {
-        public EmptyIntQueueException() { }
-    }
-    public class OverflowIntQueueException extends RuntimeException {
-        public OverflowIntQueueException() { }
-    }
+    /**
+     * Instantiates a new Int queue.
+     *
+     * @param maxlen the maxlen
+     */
     public IntQueue(int maxlen) {
         num = front = rear = 0;
         capacity = maxlen;
@@ -26,50 +30,83 @@ public class IntQueue {
             capacity = 0;
         }
     }
+
+    /**
+     * Enque int.
+     *
+     * @param x the x
+     * @return the int
+     * @throws OverflowIntQueueException the overflow int queue exception
+     */
     public int enque(int x)
-        throws OverflowIntQueueException {
+            throws OverflowIntQueueException {
         if (isFull())
             throw new OverflowIntQueueException();
         que[rear++] = x;
         num++;
         /*
-            エンキューの前のrearが配列の物理的な末尾であるときにrearをインクリメントすると
-            その値がcapacityと等しくなって配列のインデックスの上限を超えてしまう
-            そこでインクリメントした後のrearの値がキューの容量capacityと等しくなった場合は
-            rearを配列の先頭のインデックス0に戻す
+         *  エンキューの前のrearが配列の物理的な末尾であるときにrearをインクリメントすると
+         *  その値がcapacityと等しくなって配列のインデックスの上限を超えてしまう
+         *  そこでインクリメントした後のrearの値がキューの容量capacityと等しくなった場合は
+         *  rearを配列の先頭のインデックス0に戻す
          */
         if (rear == capacity)
             rear = 0;
         return x;
     }
+
+    /**
+     * Deque int.
+     *
+     * @return the int
+     * @throws EmptyIntQueueException the empty int queue exception
+     */
     public int deque()
-        throws EmptyIntQueueException {
+            throws EmptyIntQueueException {
         if (isEmpty())
             throw new EmptyIntQueueException();
         int x = que[front++];
         num--;
         /*
-            デキュー前のfrontが配列の物理的な末尾である時にfrontをインクリメントすると
-            その値がcapacityと等しくなって配列のインデックスの上限を超えてしまう
-            そこでインクリメントした後のfrontの値がキューの容量capacityと等しくなった場合は
-            frontを配列の先頭インデックス0に戻す
+         *  デキュー前のfrontが配列の物理的な末尾である時にfrontをインクリメントすると
+         *  その値がcapacityと等しくなって配列のインデックスの上限を超えてしまう
+         *  そこでインクリメントした後のfrontの値がキューの容量capacityと等しくなった場合は
+         *  frontを配列の先頭インデックス0に戻す
          */
         if (front == capacity)
             front = 0;
         return x;
     }
-    //次のデキューで取り出されるデータを覗き見するメソッドでデータの取り出しは行わないので値は更新しない
+
+    /**
+     * Peek int.
+     *
+     * @return the int
+     * @throws EmptyIntQueueException the empty int queue exception
+     */
+// 次のデキューで取り出されるデータを覗き見するメソッドでデータの取り出しは行わないので値は更新しない
     public int peek()
-        throws EmptyIntQueueException {
+            throws EmptyIntQueueException {
         if (isEmpty())
             throw new EmptyIntQueueException();
         return que[front];
     }
-    //エンキューとデキューはnumとfrontとrearの値に基づいて行われるので本体用の配列要素の値の変更は不要
+
+    /**
+     * Clear.
+     */
+// エンキューとデキューはnumとfrontとrearの値に基づいて行われるので本体用の配列要素の値の変更は不要
     public void clear() {
         num = front = rear = 0;
     }
-    //先頭から末尾側へと線形探索(キューとしての論理的な先頭要素から)
+
+    /**
+     * Index of int.
+     *
+     * @param x the x
+     * @return the int
+     */
+// 先頭から末尾側へと線形探索(キューとしての論理的な先頭要素から)
     public int indexOf(int x) {
         for (int i = 0; i < num; i++) {
             int index = (i + front) % capacity;
@@ -77,18 +114,60 @@ public class IntQueue {
         }
         return -1;
     }
+
+    /**
+     * Search int.
+     *
+     * @param x the x
+     * @return the int
+     */
+    // キューからxを探して先頭から何番目か
+    public int search(int x) {
+        for (int i = 0; i < num; i++)
+            if (que[(i + front) % capacity] == x)
+                return i + 1;
+        return 0;
+    }
+
+    /**
+     * Gets capacity.
+     *
+     * @return the capacity
+     */
     public int getCapacity() {
         return capacity;
     }
+
+    /**
+     * Size int.
+     *
+     * @return the int
+     */
     public int size() {
         return num;
     }
+
+    /**
+     * Is empty boolean.
+     *
+     * @return the boolean
+     */
     public boolean isEmpty() {
         return num <= 0;
     }
+
+    /**
+     * Is full boolean.
+     *
+     * @return the boolean
+     */
     public boolean isFull() {
         return num >= capacity;
     }
+
+    /**
+     * Dump.
+     */
     public void dump() {
         if (isEmpty())
             System.out.println("キューは空です");
@@ -96,6 +175,28 @@ public class IntQueue {
             for (int i = 0; i < num; i++)
                 System.out.print(que[(i + front) % capacity] + " ");
             System.out.println();
+        }
+    }
+
+    /**
+     * The type Empty int queue exception.
+     */
+    public static class EmptyIntQueueException extends RuntimeException {
+        /**
+         * Instantiates a new Empty int queue exception.
+         */
+        public EmptyIntQueueException() {
+        }
+    }
+
+    /**
+     * The type Overflow int queue exception.
+     */
+    public static class OverflowIntQueueException extends RuntimeException {
+        /**
+         * Instantiates a new Overflow int queue exception.
+         */
+        public OverflowIntQueueException() {
         }
     }
 }
