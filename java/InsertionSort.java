15938f8b5b2400f5fb1ee1e253a488daba3757e0
Satoru Kitaguchi <rule.the.fate.myfirststory@gmail.com>
2021-04-12 19:38:06 +0900

    [docs]単純挿入ソート(シャトルソート)

--- java/InsertionSort.java
+++ java/InsertionSort.java
@@ -1,14 +1,26 @@
+package java;
+
 import java.util.Scanner;
 
 /**
  * O(n^2)
  */
 class InsertionSort {
+
+    /**
+     * Print array.
+     *
+     * @param array the array
+     */
+    public static void printArray(int[] array) {
+        for (int j : array) System.out.print("[" + j + "]");
+        System.out.println();
+    }
+
     /**
      * 別名シャトルソート
      * 飛び越えた要素の交換が行われることがないので安定している
-     */
-    /**
+     *
      * 原列の先頭要素を目的列内の適切な位置に挿入するという操作をn - 1回繰り返す
      * 左隣の要素が現在着目している要素の値よりも大きい限り
      * その値を代入する作業を繰り返す
@@ -20,6 +32,9 @@ class InsertionSort {
      * ・jが0より大きい
      * a[j - 1]の値がtmpより大きい
      * のどちらもが成立していることとなる
+     *
+     * @param array the array
+     * @param n     the n
      */
     static void insertionSort(int[] array, int n) {
         for (int i = 1; i < n; i++) {
@@ -30,6 +45,12 @@ class InsertionSort {
             array[j] = tmp;
         }
     }
+
+    /**
+     * Insertion sort.
+     *
+     * @param array the array
+     */
     static void insertionSort(int[] array) {
         int ins, cmp, tmp;
         printArray(array);
@@ -45,35 +66,16 @@ class InsertionSort {
         }
         printArray(array);
     }
-    public static void printArray(int[] array) {
-        for (int i = 0; i < array.length; i++)
-            System.out.print("[" +array[i] + "]");
-        System.out.println();
-    }
-    public static void insertionSortTrace() {
-        int[] array = { 90, 34, 78, 12, 56 };
-        int ins, cmp, tmp;
-        printArray(array);
-        for (ins = 1; ins < array.length; ins++) {
-            System.out.printf("外側のループ: tmp <- array[%d] = %d\n", ins, array[ins]);
-            tmp = array[ins];
-            for (cmp = ins - 1; cmp >= 0; cmp--) {
-                System.out.printf("内側のループ: ins <- ins = %d, cmp = %d, tmp = %d\n", ins, cmp, tmp);
-                if (array[cmp] > tmp)
-                    array[cmp + 1] = array[cmp];
-                else
-                    System.out.println("breakで中断");
-                break;
-            }
-            System.out.printf("外側のループ: ins = %d, cmp = %d, tmp = %d\n", ins, cmp, tmp);
-            System.out.printf("外側のループ: 確定した挿入位置 = array[%d] <- tmp\n\n", cmp + 1);
-            array[cmp + 1] = tmp;
-        }
-        printArray(array);
-    }
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
         System.out.println("単純挿入ソート");
+
         System.out.print("要素数:");
         int nx = stdIn.nextInt();
         int[] x = new int[nx];
